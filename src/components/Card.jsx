5326dcaa171898772f27dd3ce914558c2c2fc626
Sergey <subsmatt@yahoo.com>
2022-10-22 11:37:51 +0100

    Prevent overrendering. Add ErrorBoundary

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
-import { useContext } from "react";
+import { useContext, memo } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 import { ToolbarContext } from "../contexts/ToolbarContext";
 import { CardContext, CardProvider } from "../contexts/CardContext";
 import CardFamiliar from "./CardFamiliar";
 import CardDelete from "./CardDelete";
+import ErrorBoundary from "./ErrorBoundary";
 
-function Card({rec, updateRecord, insertRecord, deleteRecord}){
+const CardNoErrorBoundary = memo(function CardNoErrorBoundary({rec, updateRecord, insertRecord, deleteRecord}){
     const {id, word, type, desc, tags, hits, familiar} = rec;
 
     // Get theme Context
@@ -15,6 +16,8 @@ function Card({rec, updateRecord, insertRecord, deleteRecord}){
     // Get toolbar Context
     const {showDesc} = useContext(ToolbarContext);
 
+    console.log(`card: ${id} | ${word}`);
+
     return (
         <CardProvider rec={rec} updateRecord={updateRecord} insertRecord={insertRecord} deleteRecord={deleteRecord}>
             <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 my-4" key={id}>
@@ -23,7 +26,7 @@ function Card({rec, updateRecord, insertRecord, deleteRecord}){
                         <h5>{word}</h5>
                     </div>
                     <div className="card-body">
-                        <span className="oCardDesc">{ showDesc ? desc : ""}</span>
+                        <span className="oCardDesc">{ showDesc ? desc.substr(1,3) : ""}</span>
                         <div className="oCardCat my-1">
                             <b>{type}</b>{" "}{tags.toString()}
                         </div>
@@ -33,6 +36,18 @@ function Card({rec, updateRecord, insertRecord, deleteRecord}){
                 </div>
             </div>
         </CardProvider>
+    );
+}, areEqualCard);
+
+function areEqualCard(prevProps, nextProps){
+    return (prevProps.rec.familiar === nextProps.rec.familiar);
+}
+
+function Card(props){
+    return (
+        <ErrorBoundary errorUI={<div>Something went wrond in the Card component</div>}>
+            <CardNoErrorBoundary {...props}></CardNoErrorBoundary>
+        </ErrorBoundary>
     );
 }
 
